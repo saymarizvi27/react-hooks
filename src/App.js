76b9227c61d510cbe0e849b2a5f2b33ce297894d
saymarizvi <rizvisayma@yahoo.com>
2021-05-17 23:15:30 +0530

    login logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
+
+  useEffect(() => {
+    const storedUserLoggedInfo = localStorage.getItem('isLoggedIn');
+    if (storedUserLoggedInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, [])
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
